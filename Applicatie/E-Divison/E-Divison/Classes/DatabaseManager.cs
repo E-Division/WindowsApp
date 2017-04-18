4ff2a70666e12cdf5eb9c7c97bb778aaa5f2e1a5
TarikHaci <tarikhaci@outlook.com>
2017-04-18 09:48:53 +0200

    Onnodige dingen van databasemanager

--- Applicatie/E-Divison/E-Divison/Classes/DatabaseManager.cs
+++ Applicatie/E-Divison/E-Divison/Classes/DatabaseManager.cs
@@ -15,10 +15,6 @@ namespace E_Divison.Classes
         public DatabaseManager()
         {
             Init();
-        //    if (!CheckFileExists(path).Result)
-        //    {
-        //        CreateTables();
-        //    }
         }
         private void Init()
         {
@@ -28,36 +24,9 @@ namespace E_Divison.Classes
             SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
         }
 
-        //private void CreateTables()
-        //{
-        //    //var tableQuery = "SELECT COUNT(*) FROM tbl_Page;";
-        //    //bool tableExists = con.ExecuteScalar<int>(tableQuery) == 1;
-        //    con.CreateTable<Page>();
-        //    Page page = new Page();
-        //    page.InsertPages(con);
-        //    con.CreateTable<Category>();
-        //    Category category = new Category();
-        //    category.InsertCategories(con);
-        //    con.CreateTable<Brand>();
-        //    Brand brand = new Brand();
-        //    brand.InsertBrands(con);
-        //}
         public SQLiteConnection GetCon()
         {
             return con;
-        }
-
-        private async Task<bool> CheckFileExists(string fileName)
-        {
-            try
-            {
-                var store = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
-                return true;
-            }
-            catch
-            {
-            }
-            return false;
         }
     }
 }
